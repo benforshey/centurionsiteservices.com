ec5e94787cf473ee051b645f13cc369bc0d206ac
Ben Forshey <hello@integrisweb.com>
2017-11-03 18:47:41 -0400

    comment out starter for webhook

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-exports.onPostBuild = (props) => {
-  console.log(props)
-  return console.log('build complete (from gatsby-node.js)')
-}
+// exports.onPostBuild = (props) => {
+//   console.log(props)
+//   return console.log('build complete (from gatsby-node.js)')
+// }
