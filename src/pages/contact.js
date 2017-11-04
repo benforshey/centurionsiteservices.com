8b881ffce26f5900583be42d6d4cc68db78f6289
Ben Forshey <hello@integrisweb.com>
2017-11-04 17:01:11 -0400

    update for form processing without NGINX proxy

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -45,7 +45,7 @@ class Contact extends React.Component {
     e.preventDefault()
     e.stopPropagation()
 
-    fetch('https://centurionsiteservices.com/webhook/form-mail/', {
+    fetch('http://138.197.224.215:3025/mail/', {
       method: 'post',
       body: new FormData(e.target)
     })
@@ -82,7 +82,7 @@ class Contact extends React.Component {
         <h1 className={styles.masthead}>Pricing<br /> and <br />Inquiries</h1>
         <form
           id='form'
-          action='https://centurionsiteservices.com/webhook/form-mail/'
+          action='http://138.197.224.215:3025/mail/'
           method='post'
           className={styles.form}
           onSubmit={this.handleSubmit}
